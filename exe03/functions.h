de9aa87bebba833e9f212f2ca51d8555080a9f94
Davidson <davidsonmarra@gmail.com>
2021-08-13 21:10:28 -0300

    Add exe03

--- exe03/functions.h
+++ exe03/functions.h
@@ -1,2 +1,10 @@
-
-int somar(int n1, int n2);
+void menor_maior(int v[], int n, int *menor, int *maior) {
+  * menor = v[0];
+  * maior = v[0];
+  for(int i = 1; i < n; i++) {
+    if(v[i] < * menor)
+      * menor = v[i];
+    if(v[i] > * maior)
+      * maior = v[i];
+  }
+}
--- exe03/main.c
+++ exe03/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
 int main() {
-
-    int a, b, s;
+  int menor, maior, n;
   
   // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
+  scanf("%d", &n);
+  int *vetor = (int*)malloc(n*sizeof(int));
 
   // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
+  for(int i = 0; i < n; i++) 
+    scanf("%d", &vetor[i]);
+  menor_maior(vetor, n, &menor, &maior);
 
   // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
+  printf("MENOR = %d\nMAIOR = %d", menor, maior);
 
-  return(0);
+  return 0;
 }
