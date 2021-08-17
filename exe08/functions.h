947c31475409ac2b602e0b2ec8f019ce9c0a4711
Davidson <davidsonmarra@gmail.com>
2021-08-17 10:42:27 -0300

    Add exe08

--- exe08/functions.h
+++ exe08/functions.h
@@ -1,2 +1,9 @@
-
-int somar(int n1, int n2);
+bool is_palindromo(char texto[], int aux) {
+  int tamanho = strlen(texto) - 2;
+  if(texto[aux] != texto[tamanho - aux]) 
+    return false;
+  else
+    if(aux <= (tamanho - 2) / 2)
+      is_palindromo(texto, aux + 1);
+  return true;
+}
--- exe08/main.c
+++ exe08/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "functions.h"
 
 int main() {
-
-    int a, b, s;
-  
   // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
+  int aux = 0;
+  char texto[100];
+  fgets(texto, 100, stdin);
 
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
 
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
+  // PROCESSAMENTO E SAIDA
+  if(is_palindromo(texto, aux)) 
+    printf("SIM");
+  else 
+    printf("NAO");
 
-  return(0);
+  return 0;
 }
